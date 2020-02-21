ab5e3249b5b748308ac0007a2cc6e6215797c984
Richard Hernandez <r.a.hernandez@outlook.com>
2020-02-21 16:41:43 +0000

    Add project files.

---
+++ SbrCoreRestApiWithSwaggerAndCors/Controllers/SampleController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace SbrCoreRestApiWithSwaggerAndCors.Controllers
+{
+    [ApiController]
+    [Route("api/sample")]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet()]
+        public IActionResult GetSample()
+        {
+            return Ok(new
+            {
+                Id = 100,
+                Name = "Sample Data",
+                Created = DateTime.Now.Date
+            });
+        }
+    }
+}
---
+++ SbrCoreRestApiWithSwaggerAndCors/Startup.cs
@@ -0,0 +1,89 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace SbrCoreRestApiWithSwaggerAndCors
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        readonly string SampleAllowAnyOrigins = "_sampleAllowAnyOrigins";
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+               c.SwaggerDoc("v1", new OpenApiInfo { Title = typeof(Startup).Namespace, Version = "V1" })
+               );
+
+            services.AddCors(options =>
+                options.AddPolicy(SampleAllowAnyOrigins,
+                builder =>
+                {
+                    builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                })
+            );
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler(appBuilder =>
+                {
+                    appBuilder.Run(async context =>
+                    {
+                        var exceptionHandlrFeature = context.Features.Get<IExceptionHandlerFeature>();
+                        if (exceptionHandlrFeature != null)
+                        {
+                            //do logging stuff
+                        }
+                        context.Response.StatusCode = 501;
+                        await context.Response.WriteAsync("Something went wrong. Please try later.");
+                    });
+                });
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                //app.UseHsts();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{typeof(Startup).Namespace} API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseHttpsRedirection();
+
+            app.UseCors(SampleAllowAnyOrigins);
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
